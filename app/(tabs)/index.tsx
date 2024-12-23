f007f763e47f6d2eee20d30aeea9576b64016087
Gastonfauret <Gastonfauret@gmail.com>
2024-12-23 10:26:47 -0300

    Apps Project. DONE!

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,21 @@
 import LoadingScreen from "./LoadingScreen";
+import React, { useState, useEffect } from 'react';
 import MainPage from "./main-page";
 
 
 export default function HomeScreen() { 
-  return (
-    <LoadingScreen/>
-    //<MainPage/>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simula una carga de datos
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Cambia el tiempo según sea necesario
+
+    return () => clearTimeout(timer);
+  }, []);
+
+   return isLoading ? <LoadingScreen /> : <MainPage />;
 }
 
 
